3c46c873d240952c97931d81891939978fded7a1
Reval Govender <revalgovender>
2023-02-20 00:35:15 +0000

    read json for unit counters

--- responses.py
+++ responses.py
@@ -1,43 +1,22 @@
-import random
+import json
 
 
 def get_response(message: str) -> str:
-    p_message = message.lower()
+    user_supplied_unit_name = message.lower()
 
-    if p_message == 'hello':
+    print('1')
+
+    if user_supplied_unit_name == 'hello':
         return 'Hey there!'
 
-    if p_message == '!help':
+    if user_supplied_unit_name == '!help':
         return '`This is a help message that you can modify.`'
 
-    if p_message == 'militia':
-        return 'Archers'
+    file = open('data/units.json')
+    data = json.load(file)
 
-    if p_message == 'man-at-arms' or p_message == 'Man at arms':
-        return 'Archers and Scorpions'
-
-    if p_message == 'long swordsman':
-        return 'Archers and Scorpions'
-
-    if p_message == 'two-handed swordsman':
-        return 'Archers and Scorpions'
-
-    if p_message == 'champion':
-        return 'Archers and scorpions'
-
-    if p_message == 'spearman':
-        return 'Archers and other infantry'
-
-    if p_message == 'pikeman':
-        return 'Archers, scorpions and other infantry'
-
-    if p_message == 'halberdier':
-        return'Archers, scorpions and other infantry'
-
-    if p_message == 'eagle scout':
-        return 'Infantry'
-
-    if p_message == 'eagle warrior':
-        return 'Infantry'
+    for unit in data.items():
+        if user_supplied_unit_name in unit[1]['aliases']:
+            return unit[1]['unit-data']
 
     return 'I didn\'t understand what you wrote. Try typing "!help".'
