2d7de4c9ce60a6fdd48242f2ceb0882241b1db96
Reval Govender <revalgovender>
2023-02-20 16:40:56 +0000

    refactor to use slash command

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-from src import bot
+from discord import app_commands
+import discord
+import os
+
+from src import responses
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
+    class Ralph(discord.Client):
+        def __init__(self):
+            super().__init__(intents=discord.Intents.default())
+            self_synced = False
+
+        async def on_ready(self):
+            try:
+                print('trying to sync')
+                synced = await tree.sync(guild=discord.Object(id=521077625519603712))
+                print(f"Synced {len(synced)} commands.")
+                self_synced = True
+            except Exception as exception:
+                print(exception)
+            print(f'{bot.user} is now running!')
+
+
+    bot = Ralph()
+    tree = app_commands.CommandTree(bot)
+
+
+    @tree.command(name='counter', description='Get counter to Age of Empires 2 DE unit.',
+                  guild=discord.Object(id=521077625519603712))
+    @app_commands.describe(unit="What unit do you need the counter for?")
+    async def self(interaction: discord.Interaction, unit: str):
+        response = responses.get_response(unit, str(interaction.user))
+        await interaction.response.send_message(embed=response, ephemeral=False)
+
+
+    bot.run(token=os.environ['TOKEN'])
