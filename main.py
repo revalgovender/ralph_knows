3a53d7d449a56c78c677d182d04412cdff42f05c
Reval Govender <revalgovender>
2023-02-21 14:18:37 +0000

    add autocomplete + build order command

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import json
+import typing
+import random
+
 from discord import app_commands
 import discord
 import os
+from typing import List
 
 from src import responses
 
@@ -12,9 +17,9 @@ if __name__ == '__main__':
 
         async def on_ready(self):
             try:
-                print('trying to sync')
-                synced = await tree.sync(guild=discord.Object(id=521077625519603712))
-                print(f"Synced {len(synced)} commands.")
+                # print('trying to sync')
+                # synced = await tree.sync(guild=discord.Object(id=521077625519603712))
+                # print(f"Synced {len(synced)} commands.")
                 self_synced = True
             except Exception as exception:
                 print(exception)
@@ -25,12 +30,59 @@ if __name__ == '__main__':
     tree = app_commands.CommandTree(bot)
 
 
-    @tree.command(name='counter', description='Get counter to Age of Empires 2 DE unit.',
+    async def unit_autocomplete(
+            interaction: discord.Interaction,
+            current: str,
+    ) -> typing.List[app_commands.Choice[str]]:
+        units = []
+        file = open('data/units.json')
+        data = json.load(file)
+
+        for unit in data.items():
+            units.append(unit[1]['actual-name'].lower())
+
+        # Create random 5 units to display before using starts typing unit.
+        # This will prevent Discord trying to grab the entire list of units.
+        if not current:
+            short_unit_list = random.sample(units, 5)
+            return [
+                app_commands.Choice(name=short_unit_list[0], value=short_unit_list[0]),
+                app_commands.Choice(name=short_unit_list[1], value=short_unit_list[1]),
+                app_commands.Choice(name=short_unit_list[2], value=short_unit_list[2]),
+                app_commands.Choice(name=short_unit_list[3], value=short_unit_list[3]),
+                app_commands.Choice(name=short_unit_list[4], value=short_unit_list[4]),
+            ]
+
+        # Perform filtering to limit list to items which match user input.
+        # We do this because Discord only allows a max of 25 autocomplete results.
+        # Thankfully we don't need any other limitations here as we don't have that many units.
+        units = [i for i in units if i.startswith(current)]
+
+        return [
+            app_commands.Choice(name=unit, value=unit)
+            for unit in units if current.lower() in unit.lower()
+        ]
+
+
+    @tree.command(name='counter', description='Get unit counter',
                   guild=discord.Object(id=521077625519603712))
     @app_commands.describe(unit="What unit do you need the counter for?")
+    @app_commands.autocomplete(unit=unit_autocomplete)
     async def self(interaction: discord.Interaction, unit: str):
         response = responses.get_response(unit, str(interaction.user))
-        await interaction.response.send_message(embed=response, ephemeral=False)
+        await interaction.response.send_message(embed=response, ephemeral=True)
 
 
-    bot.run(token=os.environ['TOKEN'])
+    @tree.command(name='build-order', description='Get build order',
+                  guild=discord.Object(id=521077625519603712))
+    @app_commands.describe(types="Build orders available")
+    @app_commands.choices(types=[
+        discord.app_commands.Choice(name='Fast Castle', value='1'),
+        discord.app_commands.Choice(name='Rev Man-at-Arms rush', value='2'),
+    ])
+    async def self(interaction: discord.Interaction, types: discord.app_commands.Choice[str]):
+        response = responses.get_build_order_response(int(types.value))
+        await interaction.response.send_message(embed=response, ephemeral=True)
+
+# bot.run(token=os.environ['TOKEN'])
+bot.run(token='MTA3NjQ0ODU0MjIwMzMzNDY4Ng.G-yaPF.C2OLf9z52J9uHaScc41qdwB9FQtm3n7FV6EJI8')
--- src/responses.py
+++ src/responses.py
@@ -5,21 +5,83 @@ import discord
 
 def get_response(message: str, username: str):
     user_supplied_unit_name = message.lower()
-
-    if user_supplied_unit_name == 'hello':
-        return 'Hey there!'
-
     file = open('data/units.json')
     data = json.load(file)
 
     for unit in data.items():
         if user_supplied_unit_name in unit[1]['aliases']:
-            embed = discord.Embed(title="⚔️ Counter to " + unit[1]['actual-name'], description=unit[1]['weakness'], color=0x9a8da7)
+
+            counters = unit[1]['weakness']
+
+            if counters[-1] == '.':
+                counters = counters.rstrip(counters[-1])
+
+            embed = discord.Embed(title=counters, description="are the counters for " + unit[1]['actual-name'],
+                                  color=0x9a8da7)
             return embed
 
     log_failed_request(user_supplied_unit_name, username)
 
-    embed = discord.Embed(title="😔 I couldn't find that unit sire", description='I have recorded your message to study it. I will update the scrolls if required.', color=0xf22c56)
+    embed = discord.Embed(title="😔 I couldn't find that unit sire",
+                          description='I have recorded your message to study it. I will update the scrolls if required.',
+                          color=0xf22c56)
+    return embed
+
+
+def get_build_order_response(type: int):
+    instructions = []
+    title = ''
+
+    if type == 1:
+        title = '🏰 Fast Castle'
+        instructions = [
+            "First 3 villagers build houses and then harvest sheep",
+            "6 sheep.",
+            "7 build lumber camp",
+            "7, 8 and 9th villager chop wood.",
+            "10 villager builds house and lures boar.",
+            "11 and 12 villager chop wood.",
+            "13 villager builds mill on berries.",
+            "13, 14, 15 harvest berries.",
+            "16 lures boar.",
+            "17 builds house then harvest berries.",
+            "18, 19 berries",
+            "20 builds house and then a lumber camp in another wood line(this is important)",
+            "21, 22, 23, 24 to wood in new lumber camp.",
+            "25 builds mining camp.",
+            "26, 27 gold.",
+            "Research Feudal Age.",
+            "One of villagers in wood builds barracks.",
+            "Villager 28, 29 mine gold.",
+            "One of wood villagers builds blacksmith and stable.",
+            "Research Castle age.",
+            "While going to castle research available technologies at lumber camp and mill.",
+            "2 Knights at your stable and attack!",
+        ]
+
+    if type == 2:
+        title = '⚔️ Rev Man-at-Arms Rush'
+        instructions = [
+            "First 3 villagers build houses and then harvest sheep",
+            "6 sheep.",
+            "7 build lumber camp",
+            "7, 8 and 9th villager chop wood.",
+            "10 villager builds house and lures boar.",
+            "11 and 12 villager chop wood.",
+            "13 villager builds mill on berries.",
+            "13, 14, 15 harvest berries.",
+            "16 lures boar.",
+            "17 builds house then harvest berries.",
+            "18, 19 gold",
+            "20 berries",
+            "21 stone",
+        ]
+
+    embed = discord.Embed(title=title, description='', color=0x9a8da7)
+
+    for instruction in instructions:
+        embed.add_field(name='', value='- ' + instruction, inline=False)
+
     return embed
 
 
