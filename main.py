49016dbab798489772deb747459a35bb5f080465
Reval Govender <revalgovender>
2023-04-24 12:58:08 +0100

    refactor naming + build order command to json

--- main.py
+++ main.py
@@ -23,16 +23,16 @@ if __name__ == '__main__':
         async def on_ready(self):
             try:
                 print('trying to sync')
-                synced = await tree.sync(guild=discord.Object(id=521077625519603712))
+                synced = await bot.sync(guild=discord.Object(id=521077625519603712))
                 print(f"Synced {len(synced)} commands.")
                 self_synced = True
             except Exception as exception:
                 print(exception)
-            print(f'{bot.user} is now running!')
+            print(f'{client.user} is now running!')
 
 
-    bot = Ralph()
-    tree = app_commands.CommandTree(bot)
+    client = Ralph()
+    bot = app_commands.CommandTree(client)
 
 
     async def unit_autocomplete(
@@ -69,8 +69,8 @@ if __name__ == '__main__':
         ]
 
 
-    @tree.command(name='counter', description='Get unit counter',
-                  guild=discord.Object(id=521077625519603712))
+    @bot.command(name='counter', description='Get unit counter',
+                 guild=discord.Object(id=521077625519603712))
     @app_commands.describe(unit="What unit do you need the counter for?")
     @app_commands.autocomplete(unit=unit_autocomplete)
     async def self(interaction: discord.Interaction, unit: str):
@@ -90,20 +90,20 @@ if __name__ == '__main__':
         sound.save("tts-audio.mp3")
 
         # Play mp3 in voice channel
-        vc = discord.utils.get(bot.voice_clients, guild=interaction.guild)
+        vc = discord.utils.get(client.voice_clients, guild=interaction.guild)
         if vc:
             vc.play(discord.FFmpegPCMAudio("tts-audio.mp3"))
 
 
-    @tree.command(name='build-order', description='Get build order',
-                  guild=discord.Object(id=521077625519603712))
+    @bot.command(name='build-order', description='Get build order',
+                 guild=discord.Object(id=521077625519603712))
     @app_commands.describe(types="Build orders available")
     @app_commands.choices(types=[
         discord.app_commands.Choice(name='Generic Knight rush', value='1'),
         discord.app_commands.Choice(name='Rev Man-at-Arms rush', value='2'),
     ])
     async def self(interaction: discord.Interaction, types: discord.app_commands.Choice[str]):
-        response = responses.get_build_order_response(int(types.value))
+        response = responses.get_build_order_response(types.value)
         await interaction.response.send_message(embed=response, ephemeral=False)
 
 
@@ -141,8 +141,8 @@ if __name__ == '__main__':
         ]
 
 
-    @tree.command(name='civ', description='Get civ data',
-                  guild=discord.Object(id=521077625519603712))
+    @bot.command(name='civ', description='Get civ data',
+                 guild=discord.Object(id=521077625519603712))
     @app_commands.describe(civ="What civ do you want information about?")
     @app_commands.autocomplete(civ=civ_autocomplete)
     async def self(interaction: discord.Interaction, civ: str):
@@ -150,8 +150,8 @@ if __name__ == '__main__':
         await interaction.response.send_message(embed=response, ephemeral=False)
 
 
-    @tree.command(name='join', description='Join voice channel author is in',
-                  guild=discord.Object(id=521077625519603712))
+    @bot.command(name='join', description='Join voice channel author is in',
+                 guild=discord.Object(id=521077625519603712))
     async def join(interaction: discord.Interaction):
         if interaction.user.voice:
             await interaction.user.voice.channel.connect()
@@ -160,8 +160,8 @@ if __name__ == '__main__':
             await interaction.response.send_message("You are not in a voice channel. Please join one so I can join!")
 
 
-    @tree.command(name='leave', description='Join voice channel author is in',
-                  guild=discord.Object(id=521077625519603712))
+    @bot.command(name='leave', description='Join voice channel author is in',
+                 guild=discord.Object(id=521077625519603712))
     async def join(interaction: discord.Interaction):
         if interaction.guild.voice_client:
             await interaction.guild.voice_client.disconnect()
--- src/responses.py
+++ src/responses.py
@@ -74,61 +74,13 @@ def get_civ_data(message: str):
     return embed
 
 
-def get_build_order_response(type: int):
+def get_build_order_response(build_order_type: str):
     instructions = []
     title = ''
-
-    if type == 1:
-        title = '🐎 Generic Knight Rush'
-        instructions = [
-            "First 3 villagers build houses and then harvest sheep",
-            "6 sheep.",
-            "7 build lumber camp",
-            "7, 8 and 9th villager chop wood.",
-            "10 villager builds house and lures boar.",
-            "11 and 12 villager chop wood.",
-            "13 villager builds mill on berries.",
-            "13, 14, 15 harvest berries.",
-            "16 lures boar.",
-            "17 builds house then harvest berries.",
-            "18, 19 berries",
-            "20 builds house and then a lumber camp in another wood line(this is important)",
-            "21, 22, 23, 24 to wood in new lumber camp.",
-            "25 builds mining camp.",
-            "26, 27 gold.",
-            "Research Feudal Age.",
-            "One of villagers in wood builds barracks.",
-            "Villager 28, 29 mine gold.",
-            "One of wood villagers builds blacksmith and stable.",
-            "Research Castle age.",
-            "While going to castle research available technologies at lumber camp and mill.",
-            "Train 2 Knights at your stable and attack!",
-        ]
-
-    if type == 2:
-        title = '⚔️ Rev Man-at-Arms Rush'
-        instructions = [
-            "First 3 villagers build houses and then harvest sheep",
-            "6 sheep.",
-            "7 build lumber camp",
-            "7, 8 and 9th villager chop wood.",
-            "10 villager builds house and lures boar.",
-            "11 and 12 villager chop wood.",
-            "13 villager builds mill on berries.",
-            "13, 14, 15 harvest berries.",
-            "16 lures boar.",
-            "17 builds house then harvest berries.",
-            "18, 19 gold",
-            "20 berries",
-            "21 stone",
-            "Research Feudal Age.",
-            "One of villagers in wood builds barracks.",
-            "Train 3 militia and move them towards enemy with scout",
-            "Upgrade to Man-at-Arms asap",
-            "Attack with scout and Man-at-arms"
-        ]
-
-    embed = discord.Embed(title=title, description='', color=0x9a8da7)
+    file = open('data/build-orders.json')
+    build_orders = json.load(file)
+    instructions = build_orders[build_order_type]['instructions']
+    embed = discord.Embed(title=build_orders[build_order_type]['name'], description='', color=0x9a8da7)
 
     for instruction in instructions:
         embed.add_field(name='', value='- ' + instruction, inline=False)
